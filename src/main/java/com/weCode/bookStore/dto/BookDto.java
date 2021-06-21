a35762c915d6a3d8fdfdd2c9909d87f0997cc3a9
Segun Abolaji <talkense@gmail.com>
2021-06-21 18:09:03 +0100

    Initial commit

---
+++ src/main/java/com/weCode/bookStore/dto/BookDto.java
@@ -0,0 +1,22 @@
+package com.weCode.bookStore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDto {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
